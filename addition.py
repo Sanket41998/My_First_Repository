3940d94e27f8040e1243e7348d2f770ea8100d53
Sanket41998 <sanketwaikar5@gmail.com>
2024-01-01 22:34:04 +0530

    this is my second commit

--- addition.py
+++ addition.py
@@ -2,3 +2,18 @@ a=50
 b=60
 c=a+b
 print(c)
+
+class name:
+    def first(self):
+        print("sanket")
+class age(name):
+    def A(self):
+        print(32)
+class country(age):
+    def place(self):
+        print("india")
+obj1=country()
+obj1.place()
+obj2=age()
+obj2.first()
+obj2.A()
